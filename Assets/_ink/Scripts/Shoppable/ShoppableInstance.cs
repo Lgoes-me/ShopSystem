44fcce6ca991f1453c79e8a6e700494ccc7d1fbb
Lucas <lucas.rodrigues.goes@gmail.com>
2020-08-16 16:21:52 -0300

    Completing the shop system

--- Assets/_ink/Scripts/Shoppable/ShoppableInstance.cs
+++ Assets/_ink/Scripts/Shoppable/ShoppableInstance.cs
@@ -1,12 +1,12 @@
-using UnityEngine;
-
 namespace ink.Shop
 {
     public class ShoppableInstance : ShoppableBehaviour
     {
+        public Item currentState;
+
         public override void AddBoughtItem(Item item)
         {
-
+            currentState = item;
         }
     }
 }
--- Assets/_ink/Scripts/Shoppable/ShoppableInventory.cs
+++ Assets/_ink/Scripts/Shoppable/ShoppableInventory.cs
@@ -1,12 +1,19 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 namespace ink.Shop
 {
     public class ShoppableInventory : ShoppableBehaviour
     {
+        public List<Item> itens;
+
+        public void Awake()
+        {
+            itens = new List<Item>();
+        }
+
         public override void AddBoughtItem(Item item)
         {
-
+            itens.Add(item);
         }
     }
 }
