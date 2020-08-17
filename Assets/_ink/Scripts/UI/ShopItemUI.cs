e8b7e2015ae2ad5ef41110f11a1e16c3ae005b31
Lucas <lucas.rodrigues.goes@gmail.com>
2020-08-17 11:56:47 -0300

    name of the folders, ui and prefabs

---
+++ Assets/_ink/Scripts/UI/ShopItemUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace ink.Shop
+{
+    public class ShopItemUI : MonoBehaviour
+    {
+        public Shop shop;
+        public Item item;
+        public ShoppableBehaviour shopable;
+
+        public Image image;
+        public TMP_Text Name;
+        public TMP_Text Value;
+
+        public void OnEnable()
+        {
+            if(item.itemSprite != null)
+            {
+                image.sprite = item.itemSprite;
+            }
+            
+            if (item.itemName != null)
+            {
+                Name.text = item.itemName;
+            }
+
+            if (item.price != 0)
+            {
+                Name.text = item.price.ToString("00"); ;
+            }
+        }
+
+        public void Buy()
+        {
+            if (shop.CheckAndPay(item.price))
+            {
+                shopable.AddBoughtItem(item);
+            }
+        }
+    }
+}
