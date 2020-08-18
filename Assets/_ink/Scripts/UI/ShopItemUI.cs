96227a871228549c60ef1885cd413e30c19bc9cd
Lucas <lucas.rodrigues.goes@gmail.com>
2020-08-18 00:01:29 -0300

    Updated with the option to ammount of diferent itens on the shop

--- Assets/_ink/Scripts/UI/ShopItemUI.cs
+++ Assets/_ink/Scripts/UI/ShopItemUI.cs
@@ -9,34 +9,68 @@ namespace ink.Shop
         public Shop shop;
         public Item item;
         public ShoppableBehaviour shopable;
+        public int ammountOfItensOnShop = 1;
 
+        public Button button;
         public Image image;
         public TMP_Text Name;
+        public TMP_Text Ammount;
         public TMP_Text Value;
 
         public void OnEnable()
         {
-            if(item.itemSprite != null)
+            UpdateDisplay();
+        }
+
+        public void UpdateDisplay()
+        {
+            if (item.itemSprite != null && image != null)
             {
                 image.sprite = item.itemSprite;
             }
-            
-            if (item.itemName != null)
+
+            if (item.itemName != null && Name != null)
             {
                 Name.text = item.itemName;
             }
 
-            if (item.price != 0)
+            if (item.price != 0 && Value != null)
             {
-                Value.text = item.price.ToString("00"); ;
+                Value.text = item.price.ToString("0");
+            }
+
+            if (ammountOfItensOnShop > 0 && Ammount != null)
+            {
+                Ammount.text = ammountOfItensOnShop.ToString("0");
+            }
+            else
+            {
+                Ammount.gameObject.SetActive(false);
+            }
+            
+            if (ammountOfItensOnShop == 0)
+            {
+                button.interactable = false;
+                Ammount.gameObject.SetActive(false);
             }
         }
 
         public void Buy()
         {
+            if (ammountOfItensOnShop == 0)
+            {
+                button.interactable = false;
+                Ammount.gameObject.SetActive(false);
+                return;
+            }
+
             if (shop.CheckAndPay(item.price))
             {
                 shopable.AddBoughtItem(item);
+
+                ammountOfItensOnShop -= 1;
+
+                UpdateDisplay();
             }
         }
     }
